a2487396da8f8dc3fe0baccd1963a3280f61262e
Sarbajit Paul <sarbajitpaul1996@gmail.com>
2018-04-24 16:37:44 +0530

    Resolve some issues
    The following are the results after resolving:
    - Submit rating using stars
    - Make submitted ratings non-clickable
    - Make rating bars take up full width in small screens
    - Mark rating bars with star numbers
    - Add footer
    - Change icon of Add to Cart button
    - Change navbar items for both logged in and logged out situations

--- Web-Content/js/product.js
+++ Web-Content/js/product.js
@@ -1,1 +1,34 @@
-$(".button-collapse").sideNav();
+$( document ).ready(function(){
+  $(".button-collapse").sideNav();
+  $(".dropdown-button").dropdown();
+});
+
+var star = document.getElementsByClassName("rating-star");
+
+for(i = 0 ; i < star.length ; i++){
+  star[i].addEventListener("mouseover", applyColor);
+  star[i].addEventListener("mouseout", removeColor);
+  star[i].addEventListener("click", getRating);
+}
+
+function applyColor(){
+  var num = this.id.substring(5,6);
+
+  for(i = 0 ; i < num ; i++){
+    star[i].style.color = "orange";
+  }
+}
+
+function removeColor(){
+  var i = document.getElementById("fb-rating-id").value;
+
+  for( ; i < star.length ; i++){
+    star[i].style.color = "lightgrey";
+  }
+}
+
+function getRating(){
+  var num = this.id.substring(5,6);
+  document.getElementById('fb-rating-id').value = num;
+  removeColor();
+}
